7e137e20a793726ad913b42dad2ec0817ebf49be
hatezamr <145778168+hatezamr@users.noreply.github.com>
2023-10-13 18:09:20 +0400

    Update nikita.h

--- nikita.h
+++ nikita.h
@@ -3,32 +3,65 @@
 class Platform
 {
     public:
-    int platformX, platformY;
+    int x, y;
     string file;
-    int platformW, platformH;
+    int w, h;
     HDC  image;
 
     Platform(int x1, int y1, string file1, int w1, int h1)
     {
-        platformX=x1;platformY=y1;file=file1;platformW=w1;platformH=h1;
+        x=x1;y=y1;file=file1;w=w1;h=h1;
         image = txLoadImage(file.c_str());
-
-    }
-
-    int OnPlatform(int x1)
-    {
-        if (x1>platformX && x1<platformX+platformW) return 1;
-        return 0;
 
     }
 
     int Draw()
     {
-    txTransparentBlt(txDC(),   platformX,   platformY, platformW, platformH,image,0,0,TX_WHITE);
+    txTransparentBlt(txDC(),   x,   y, w, h,image,0,0,TX_WHITE);
+
+    }
+
+    int OnPlatform(int x1)
+    {
+        if (x1>x && x1<x+w) return 1;
+        return 0;
+
+    }
+
+      int MoveLeft(int n)
+    {
+    x=x-n;
+
     }
 
 
+
+
 };
+
+class Background1
+{
+    public:
+     int x, y;
+    string file;
+    int w, h;
+    HDC  image;
+ Background1(HDC b , int w1 , int h1){
+ x=0;y=0;image=b;w=w1;h=h1;}
+
+int Draw()
+{
+   txBitBlt (txDC(),   0,   0, w, h,land,0,0);
+
+};
+
+    int MoveLeft(int b)
+    {
+    x=x-b;
+
+    }
+};
+
 
 int Background(HDC land, int w,int h)
 {
